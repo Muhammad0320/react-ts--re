c9031857f9802cc05119dd8abc566ed05cf8d486
Balogun Muhammad Awwal <balogunmuhammadawwal@gmail.com>
2024-06-22 08:56:44 -0700

    chore:  update app

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,14 @@
 import CourseGoals from "./components/CourseGoals.tsx";
+import Header from "./components/Header.tsx";
+import goalsImg from "./assets/goals.jpg";
 
 export default function App() {
   return (
     <main>
+      <Header image={{ src: goalsImg, alt: "A list of goals" }}>
+        <h1> Your course goals </h1>
+      </Header>
+
       <CourseGoals title="Learn React + TS">
         {" "}
         Learn it from ground up, step by step{" "}
