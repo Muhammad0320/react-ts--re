76a73f210b6b364e14ebbbdb20f82a51ead19955
Balogun Muhammad Awwal <balogunmuhammadawwal@gmail.com>
2024-06-22 07:52:22 -0700

    chore:  update course goals

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,12 @@
+import { CourseGoals } from "./components/CourseGoals.tsx";
+
 export default function App() {
-  return <h1>Let's get started!</h1>;
+  return (
+    <main>
+      <CourseGoals
+        title="Learn React + TS"
+        description="Learn it from ground up, step by step"
+      />
+    </main>
+  );
 }
--- src/components/CourseGoals.tsx
+++ src/components/CourseGoals.tsx
@@ -1,10 +1,15 @@
-export function CourseGoals() {
+interface CourseGoalTypes {
+  title: string;
+  description: string;
+}
+
+export function CourseGoals({ title, description }: CourseGoalTypes) {
   return (
     <article>
       <div>
-        <h2>TITLE</h2>
+        <h2> {title} </h2>
 
-        <p> DESCRIPTION </p>
+        <p> {description} </p>
       </div>
 
       <button> DELETE </button>
