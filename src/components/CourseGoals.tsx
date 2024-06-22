0fe2a27ddb393e4278ba3a5e998600edc664462b
Balogun Muhammad Awwal <balogunmuhammadawwal@gmail.com>
2024-06-22 07:35:51 -0700

    chore: update goals

---
+++ src/components/CourseGoals.tsx
@@ -0,0 +1,13 @@
+export function CourseGoals() {
+  return (
+    <article>
+      <div>
+        <h2>TITLE</h2>
+
+        <p> DESCRIPTION </p>
+      </div>
+
+      <button> DELETE </button>
+    </article>
+  );
+}
