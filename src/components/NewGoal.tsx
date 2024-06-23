fd5ee55bf656a4d1ea401385dea0a049218c8b4a
Balogun Muhammad Awwal <balogunmuhammadawwal@gmail.com>
2024-06-22 21:12:44 -0700

    chore:  update new goal

--- src/components/NewGoal.tsx
+++ src/components/NewGoal.tsx
@@ -1,8 +1,22 @@
-import { type FormEvent } from "react";
+import { FC, useRef, type FormEvent } from "react";
 
-const NewGoal = () => {
-  const handleSubmit = (e: FormEvent) => {
+type NewGoalsProp = {
+  onAddGoal(title: string, summary: string): void;
+};
+
+const NewGoal: FC<NewGoalsProp> = ({ onAddGoal }) => {
+  const goalRef = useRef<HTMLInputElement>(null);
+
+  const summaryRef = useRef<HTMLInputElement>(null);
+
+  const handleSubmit = (e: FormEvent<HTMLFormElement>) => {
     e.preventDefault();
+
+    const goal = goalRef.current!.value;
+
+    const summay = summaryRef.current!.value;
+
+    onAddGoal(goal, summay);
   };
 
   return (
@@ -10,13 +24,13 @@ const NewGoal = () => {
       <p>
         <label htmlFor="title">Your Goal</label>
 
-        <input type="text" name="title" id="title" />
+        <input type="text" name="title" id="title" ref={goalRef} />
       </p>
 
       <p>
         <label htmlFor="summary">Summary</label>
 
-        <input type="text" name="summary" id="summary" />
+        <input type="text" name="summary" id="summary" ref={summaryRef} />
       </p>
 
       <p>
