9bef31465335fc2209e7842314bbfe5ac7cf75a3
Balogun Muhammad Awwal <balogunmuhammadawwal@gmail.com>
2024-06-23 06:35:42 -0700

    chore: update info box

---
+++ src/components/InfoBox.tsx
@@ -0,0 +1,23 @@
+import { FC, PropsWithChildren } from "react";
+
+type InfoBoxTypes = PropsWithChildren<{ mode: "hint" | "warning" }>;
+
+const InfoBox: FC<InfoBoxTypes> = ({ children, mode }) => {
+  if (mode == "hint") {
+    return (
+      <aside className="infobox infobox-hint">
+        <p> {children} </p>
+      </aside>
+    );
+  }
+
+  return (
+    <aside className="infobox infobox-warning warning--medium">
+      <h2> Warning </h2>
+
+      <p> {children} </p>
+    </aside>
+  );
+};
+
+export default InfoBox;
