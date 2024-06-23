40e81651b1445129e183af713b4d5b3e0d883377
Balogun Muhammad Awwal <balogunmuhammadawwal@gmail.com>
2024-06-22 20:41:23 -0700

    chore: update course goals

--- src/components/CourseGoals.tsx
+++ src/components/CourseGoals.tsx
@@ -1,8 +1,17 @@
 import { FC, type PropsWithChildren } from "react";
 
-type CourseGoalTypes = PropsWithChildren<{ title: string }>;
+type CourseGoalTypes = PropsWithChildren<{
+  title: string;
+  id: number;
+  onDelete(id: number): void;
+}>;
 
-const CourseGoals: FC<CourseGoalTypes> = ({ title, children }) => {
+const CourseGoals: FC<CourseGoalTypes> = ({
+  title,
+  children,
+  id,
+  onDelete,
+}) => {
   return (
     <article>
       <div>
@@ -11,9 +20,9 @@ const CourseGoals: FC<CourseGoalTypes> = ({ title, children }) => {
         <p> {children} </p>
       </div>
 
-      <button> DELETE </button>
+      <button onClick={() => onDelete(id)}> DELETE </button>
     </article>
   );
 };
 
-export { CourseGoals };
+export default CourseGoals;
