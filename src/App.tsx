c1fe59253733d43dbdc3321d0201082432eb0753
Balogun Muhammad Awwal <balogunmuhammadawwal@gmail.com>
2024-06-22 20:19:34 -0700

    chore: update apptsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,38 @@
 import CourseGoals from "./components/CourseGoals.tsx";
 import Header from "./components/Header.tsx";
 import goalsImg from "./assets/goals.jpg";
+import { useState } from "react";
+
+type GoalType = {
+  title: string;
+  description: string;
+};
 
 export default function App() {
+
+
+  const [goals, setGoal] = useState<GoalType[]>([]);
+
+  const handleAddGoal = () => {
+    const newGoal: GoalType = {
+      title: "Master the art of deepwork",
+      description: "Use every worthy technique of it",
+    };
+
+    setGoal((prevGoals) => [...prevGoals, newGoal]);
+  };
+
+
   return (
     <main>
       <Header image={{ src: goalsImg, alt: "A list of goals" }}>
         <h1> Your course goals </h1>
       </Header>
+      <button onClick={handleAddGoal}> Add goal </button>
 
-      <CourseGoals title="Learn React + TS">
-        {" "}
-        Learn it from ground up, step by step{" "}
-      </CourseGoals>
+      {goals.map(({ title, description }) => (
+        <CourseGoals title={title}> {description}</CourseGoals>
+      ))}
     </main>
   );
 }
